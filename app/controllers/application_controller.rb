98c4474d51fbcd41093368ca6ca16055eae53bab
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-11-18 00:22:27 -0800

    Removed login/logout methods in user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
   private
 
   def find_user
-    if session[:user_id]
-      @login_user = User.find_by(id: session[:user_id])
+    #return user matching id from session variable
+    return @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
     end
   end
-end
+
